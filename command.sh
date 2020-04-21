c56764abea8200a801e3edfb417a5a5b41bcf04c
Ko Takagi <knockoutmarch@gmail.com>
2020-04-21 14:09:29 +0900

    :ant: #1 Fixed garbled characters in Mailhog

--- command.sh
+++ command.sh
@@ -28,5 +28,9 @@ wp --allow-root comment delete 1 --force
 wp --allow-root plugin install wp-multibyte-patch --activate
 wp --allow-root plugin install query-monitor --activate
 
+# Mailhog メールが文字化けしないよう WP Multibyte Patch の設定調整
+# https://eastcoder.com/code/wp-multibyte-patch/
+echo -e "<?php\n\$wpmp_conf['mail_mode'] = 'UTF-8';\n\$wpmp_conf['patch_wp_mail'] = false;" >> wp-content/wpmp-config.php
+
 # WordPress 日本語コアファイルをアップデート
 wp --allow-root core update --locale=ja --force
